cf63428e3e58401cf860736da7bf5837d45d7f57
Taras <taras.glushich@gmail.com>
2023-12-18 16:39:03 +0200

    modified script cicles

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('One of last yours wached movies?', ''),
-    b = prompt('How you estimate this?', ''),
-    c = prompt('One of last yours wached movies?', ''),
-    d = prompt('How you estimate this?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+for (let i = 0; i < 2; i++) {
+    a = prompt('One of last yours wached movies?', ''),
+        b = prompt('How you estimate this?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Not enought");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a good");
+} else if (personalMovieDB.count >= 30) {
+    console.log("You are cinemaman");
+} else {
+    console.log("error");
+}
 console.log(personalMovieDB);
